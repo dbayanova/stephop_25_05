dbe131e33e0b7be6d39a2351cbe65fd6ef992a7c
Баянова Дана Бауржановна <@itstep.loc>
2022-05-31 17:33:29 +0600

    add lesson_03 files

--- stepshop/stepshop/urls.py
+++ stepshop/stepshop/urls.py
@@ -19,9 +19,9 @@ from stepshop.views import index, contact, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('contact/', contact),
-    path('about/', about),
-    path('products/', include('mainapp.urls')),
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+    path('products/', include('mainapp.urls', namespace='products')),
 
 ]
--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    title = 'Главная страница'
+
+    context = {
+        'title': title,
+    }
+
+    return render(request, 'index.html', context)
+
+    # return render(request=request, template_name='index.html', context=context)
 
 
 def about(request):
-    return render(request, 'about.html')
+    title = 'О нас'
+    context = {
+        'title': title,
+    }
+
+    return render(request, 'about.html', context)
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    title = 'Контакты'
+    context = {
+        'title': title
+    }
+
+    return render(request, 'contact.html', context)
