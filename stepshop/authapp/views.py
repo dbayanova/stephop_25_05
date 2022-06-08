d2890fbd80e95cb987b73624f0f1342ddda42d8c
Баянова Дана Бауржановна <@itstep.loc>
2022-06-08 17:49:34 +0600

    lesson_07 files

--- stepshop/authapp/views.py
+++ stepshop/authapp/views.py
@@ -3,12 +3,21 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
 
 
 def login(request):
     title = 'Вход'
     login_form = ShopUserLoginForm(data=request.POST)
+
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
 
     context = {
         'title': title,
@@ -16,6 +25,26 @@ def login(request):
     }
 
     return render(request, 'authapp/login.html', context)
+
+
+def register(request):
+    title = 'Регистрация'
+
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {
+        'title': title,
+        'register_form': register_form,
+    }
+
+    return render(request, 'authapp/register.html', context)
 
 
 def logout(request):
